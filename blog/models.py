dc3ccd31fc9c4f90bc4c0922f5f524e106420ccd
Christian Eulogio <chrtaw@outlook.com>
2021-09-19 19:53:52 -0500

    aprendimos post en admin,en views  y urls

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title=models.CharField(max_length=250)
+    content=models.TextField()
+
+    #para ponerle el nombre del titulo
+    def __str__(self):
+        return self.title
+    
